c436a0ba738d5c3ade04f0e585eb02290e032096
dippyhippy <ben@dippyhippy.com>
2014-12-27 05:46:16 +0000

    stuff

---
+++ components/BranchOnMode.js
@@ -0,0 +1,19 @@
+var noflo = require('noflo');
+
+exports.getComponent = function () {
+  var c = new noflo.Component();
+
+  c.inPorts.add('mode', function (event, payload) {
+    if (event !== 'data') {
+      return;
+    }
+    // Do something with the packet, then
+    if (payload === 'server' {
+    	c.outPorts.out.send(payload);
+    }    
+  });
+  c.outPorts.add('server');
+  c.outPorts.add('browser');
+  c.outPorts.add('client');
+  return c;
+};
